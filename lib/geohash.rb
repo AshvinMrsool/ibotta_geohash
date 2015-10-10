1c34e72e8180d533038fff5c5e9f76dbc7b46d5b
Justin Hart <jhart@onyxraven.com>
2015-10-10 11:07:15 -0700

    fixes, tests

--- lib/geohash.rb
+++ lib/geohash.rb
@@ -1,38 +1,32 @@
-=begin
-geohash.rb
-Pure ruby GeoHashing library, updated and cleaned up by Justin Hart @ Ibotta.com
-
-
-Based on:
-https://github.com/masuidrive/pr_geohash
-Geohash library for pure ruby
-Distributed under the MIT License
-
-Based on:
-// http://github.com/davetroy/geohash-js/blob/master/geohash.js
-// geohash.js
-// Geohash library for Javascript
-// (c) 2008 David Troy
-// Distributed under the MIT License
-=end
-
-# TODO YARD style
 # TODO document encode/decode
 # TODO perf test
 # TODO compare to C impl
-# TODO import boundary fixes + tests
+
+#Pure ruby geohash library
+#
+# Based on: pr_geohash
+# https://github.com/masuidrive/pr_geohash
+# Yuichiro MASUI
+# Geohash library for pure ruby
+# Distributed under the MIT License
+#
+# Based library is
+#// http://github.com/davetroy/geohash-js/blob/master/geohash.js
+#// geohash.js
+#// Geohash library for Javascript
+#// (c) 2008 David Troy
+#// Distributed under the MIT License
 #
 
 module GeoHash
-  VERSION = "0.1"
+  VERSION = "0.2"
 
-  # Decode bounding box from geohash string
-  #
-  # == Parameters:
-  # geohash:: geohash string
-  #
-  # == Returns:
-  # decoded bounding box [[south latitude, west longitude],[north latitude, east longitude]]
+  #decode bounding box from geohash string
+  # @todo  reorder bounds?
+  # @todo  see if faster way (less array access?)
+  # @todo  see if split() faster than scan()
+  # @param [String] geohash string
+  # @return [Array<Array>] decoded bounding box [[south latitude, west longitude],[north latitude, east longitude]]
   def decode(geohash)
     latlng = [[-90.0, 90.0], [-180.0, 180.0]]
     is_lng = 1
@@ -46,15 +40,22 @@ module GeoHash
   end
   module_function :decode
 
-  # Encode latitude and longitude into geohash
-  #
-  # == Parameters:
-  # latitude:: float latitude point
-  # longitude:: float longitude point
-  # precision:: number of characters as precision
-  #
-  # == Returns:
-  # encoded geohash string
+  #decode center of geohash area
+  # @todo  see if faster way? other libs do this first, calc bounding from it
+  # @param [String] geohash string
+  # @return [Array<Float>] latitude, longitude of center
+  def decode_center(geohash)
+    res = decode(geohash)
+    [((res[0][0] + res[1][0]) / 2), ((res[0][1] + res[1][1]) / 2)]
+  end
+  module_function :decode_center
+
+  #Encode latitude and longitude into geohash string
+  # @todo  see if faster way? less array access?
+  # @param [Float] latitude
+  # @param [Float] longitude
+  # @param [Integer] precision number of characters
+  # @return [String] encoded
   def encode(latitude, longitude, precision=12)
     latlng = [latitude, longitude]
     points = [[-90.0, 90.0], [-180.0, 180.0]]
@@ -73,60 +74,54 @@ module GeoHash
   module_function :encode
 
   # Calculate each 8 direction neighbors as geohash
-  #
-  # == Parameters:
-  # geohash:: string center geohash
-  #
-  # == Returns:
-  # array of neighbors from top clockwise (top, topright, right...)
+  # @param [String] geohash
+  # @return [Array<String>] neighbors in clockwise (top, topright, right, ...). Invalid regions at the poles are excluded
   def neighbors(geohash)
     [[:top, :right], [:right, :bottom], [:bottom, :left], [:left, :top]].map do |dirs|
       point = adjacent(geohash, dirs[0])
       [point, adjacent(point, dirs[1])]
-    end.flatten
+    end.flatten.compact
   end
   module_function :neighbors
 
-  # calculate one adjacent neighbor
-  #
-  # == Parameters:
-  # geohash:: string origin geohash
-  # dir:: which direction to get a geohash of (only top, right, bottom, left)
-  #
-  # == Returns:
-  # string geohash of neighbor
+  #calculate one adjacent neighbor
+  # @param [String] geohash
+  # @param [Symbol] dir which direction (top, right, bottom left)
+  # @return [String] neighbor in that direction (or nil if invalid)
   def adjacent(geohash, dir)
-    if geohash == ''
-      geohash
-    else
+    return if geohash.nil? || geohash.empty?
       base, lastChr = geohash[0..-2], geohash[-1,1]
       type = (geohash.length % 2)==1 ? :odd : :even
       if BORDERS[dir][type].include?(lastChr)
         base = adjacent(base, dir)
+        if base.nil?
+          return if dir == :top || dir == :bottom
+          base = ''
+        end
       end
       base + BASE32[NEIGHBORS[dir][type].index(lastChr),1]
-    end
   end
   module_function :adjacent
 
+  #get geohashes covering a radius
+  # @param [Float] lat
+  # @param [Flaot] lon
+  # @param [Float] radius_meters
+  # @return [Array<String>] hashes (neighbors and center) covering radius in clockwise (top, topright, right, ...). neighbors not covering radius are excluded
   def areas_by_radius(lat, lon, radius_meters)
     #get min/max latitude and longitude of radius around point
     min_lat, min_lon, max_lat, max_lon = radius_box = bounding_box(lat, lon, radius_meters)
-    p radius_box
 
     #estimate the size of boxes to target
     steps = estimate_steps_by_radius(radius_meters)
     #re-encode point at steps
     hash = encode(lat, lon, steps)
-    p hash
 
     #get neighbors of box
     n_n, n_ne, n_e, n_se, n_s, n_sw, n_w, n_nw = nb = neighbors(hash)
-    p nb
 
     #get original bounding box
     s, w, n, e = area_box = decode(hash).flatten
-    p area_box
 
     if s < min_lat
       #area already covers south bounds of target
@@ -161,6 +156,9 @@ module GeoHash
   end
   module_function :areas_by_radius
 
+  #estimate steps needed to cover radius
+  # @param [Float] radius_meters
+  # @return [Integer] steps required
   def estimate_steps_by_radius(radius_meters)
     step = 1
     v = radius_meters
@@ -173,6 +171,12 @@ module GeoHash
   end
   module_function :estimate_steps_by_radius
 
+  #get bounding box around a radius (adjusted for latitude)
+  # @todo  reorder return to match decode?
+  # @param [Float] lat
+  # @param [Float] lon
+  # @param [Flaot] radius_meters
+  # @return [Array] min_lat, min_long, max_lat, max_long
   def bounding_box(lat, lon, radius_meters)
     radius_meters  = radius_meters.to_f
     delta_lat = radius_meters / (111320.0 * Math.cos(lat))
